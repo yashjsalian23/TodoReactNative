6d2a81a4c36ee30b7fcf2ab24c731c41b80e14c8
yashjsalian23 <yashjsalian23@gmail.com>
2020-07-10 20:05:30 +0530

    added create course goal app

--- App.js
+++ App.js
@@ -1,23 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+//import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  let [enterdGoal, setEnteredGoal] = useState('');
+  let [courseGoals, setCourseGoals] = useState([]);
+
+  let inputChangeHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  }
+
+  let addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enterdGoal]);
+  }
+
   return (
-    <View style={{padding: 30}}>
+    <View style={styles.container}>
       <View>
-        <TextInput placeholder="Goal Title" style={{borderBottomColor: "black", borderBottomWidth: 2, marginBottom: 10}} />
-        <Button title="add" />
-      </View>   
+        <TextInput 
+          placeholder="Goal Title" 
+          style={styles.textField}
+          onChangeText={inputChangeHandler} />
+
+        <Button title="add" onPress={addGoalHandler} />
+      </View>  
+      <View>
+        {courseGoals.map(item => <Text key={Math.random()}>{item}</Text>)}
+      </View> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30
   },
+  textField: {
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    marginBottom: 10
+  }
 });
